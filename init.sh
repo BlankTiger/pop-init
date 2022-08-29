0be4a55edfff8bfd38a73ed0fa4d857c273c9faf
Maciej Urban <maciej-urban@outlook.com>
2022-08-30 00:46:43 +0200

    All packages are added I think

--- init.sh
+++ init.sh
@@ -4,8 +4,11 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim curl build-essential cmake gcc python3 clang snapd ripgrep zsh wget make alacritty tmux neofetch \
-	rsync python3-pip mpv bat gimp inkscape bottom golang-go
+sudo apt-get install neovim curl build-essential cmake gcc \
+	python3 clang snapd ripgrep zsh wget make alacritty tmux \
+	neofetch rsync python3-pip mpv bat gimp inkscape bottom \
+	golang-go gnome-tweaks exa dotenv docker.io automake
+
 env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
 curl https://raw.githubusercontent.com/thameera/vimv/master/vimv > ~/bin/vimv && chmod +755 ~/bin/vimv
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
